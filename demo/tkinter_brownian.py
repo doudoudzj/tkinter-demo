54ba2a5a3023d7fcf521e888214bbbd41e1e68bc
唐钰豆豆 <doudoudzj@sina.com>
2018-06-05 20:49:19 +0800

    update

---
+++ demo/tkinter_brownian.py
@@ -0,0 +1,62 @@
+# Brownian motion -- an example of a multi-threaded Tkinter program.
+# python开发_thread_布朗运动
+# https://www.cnblogs.com/hongten/p/hongten_python_thread_tkinter_brownian.html
+
+# bug, 退出异常, 不能退出
+# 下面我将给大家介绍有关python中thread来实现布朗运动的一个例子
+
+
+from tkinter import *
+import random
+import threading
+import time
+import sys
+
+#画布大小
+WIDTH = 400
+HEIGHT = 300
+SIGMA = 10
+BUZZ = 2
+RADIUS = 2
+LAMBDA = 10
+FILL = 'red'
+
+stop = 0  # Set when main loop exits
+
+
+def particle(canvas):
+    r = RADIUS
+    x = random.gauss(WIDTH / 2.0, SIGMA)
+    y = random.gauss(HEIGHT / 2.0, SIGMA)
+    p = canvas.create_oval(x - r, y - r, x + r, y + r, fill=FILL)
+    while not stop:
+        dx = random.gauss(0, BUZZ)
+        dy = random.gauss(0, BUZZ)
+        dt = random.expovariate(LAMBDA)
+        try:
+            canvas.move(p, dx, dy)
+        except TclError:
+            break
+        time.sleep(dt)
+
+
+def main():
+    global stop
+    root = Tk()
+    root.title('布朗运动')
+    canvas = Canvas(root, width=WIDTH, height=HEIGHT)
+    canvas.pack(fill='both', expand=1)
+    # 粒子数目
+    np = 30
+    if sys.argv[1:]:
+        np = int(sys.argv[1])
+    for i in range(np):
+        t = threading.Thread(target=particle, args=(canvas, ))
+        t.start()
+    try:
+        root.mainloop()
+    finally:
+        stop = 1
+
+
+main()
