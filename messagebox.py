e9545ef39da9d038d08a527cd1d065d5733491e9
唐钰豆豆 <doudoudzj@sina.com>
2018-06-11 22:39:09 +0800

    update

---
+++ messagebox.py
@@ -0,0 +1,10 @@
+import tkinter.messagebox
+from tkinter import *
+tkinter.messagebox.askokcancel("FishC Demo", '发射导弹？')
+tkinter.messagebox.askquestion("FishC Demo", "买个优盘？")
+tkinter.messagebox.askretrycancel("FishC Demo", "启动失败，重试？")
+tkinter.messagebox.askyesno("FishC Demo", "我帅吗?")
+tkinter.messagebox.showerror("FishC Demo", "出错啦！")
+tkinter.messagebox.showinfo("FishC Demo", "2017新年快乐")
+tkinter.messagebox.showwarning("FishC Demo", "你在偷懒！")
+mainloop()
