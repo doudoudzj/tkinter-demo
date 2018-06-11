7898eab33afbbdbfaa0b816a7fc938c3e2ab5224
唐钰豆豆 <doudoudzj@sina.com>
2018-06-11 21:06:48 +0800

    update

--- search-music/main.py
+++ search-music/main.py
@@ -1,45 +1,45 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
-from tkinter import *
+import tkinter
+import tkinter.messagebox
+
 import search
 
-root = Tk()  # 创建窗口对象的背景色
-
+root = tkinter.Tk()  # 创建窗口对象的背景色
 
 # mp3 = search.searchMusic(searchInputKey)
 
-
 # 搜索框
-searchInputKey = StringVar()
-searchInput = Entry(root, textvariable=searchInputKey)
+searchInputKey = tkinter.StringVar()
+searchInput = tkinter.Entry(root, textvariable=searchInputKey)
 searchInput.pack()
+
 
 # 搜索指令
 def searchCommand():
-    mp3 = search.searchMusic(searchInputKey.get())
-    for item in mp3:  # 第二个小部件插入数据
-        searchList.insert(0, item)
+    val = searchInput.get()
+    print(val)
+    print(val.strip())
+    if val.strip() == "":
+        print('请输入关键字')
+        tkinter.messagebox.showinfo("提示", "请输入关键字")
+    else:
+        mp3 = search.searchMusic(val)
+        for item in mp3:  # 列表插入数据
+            searchList.insert(0, item)
 
-# 点击搜索
-searchButton = Button(root, text="搜索", command=searchCommand)
+
+# 搜索按钮
+searchButton = tkinter.Button(root, text="搜索", command=searchCommand)
 searchButton.pack()
 
 # 搜到的列表
-searchList = Listbox(root)
+searchList = tkinter.Listbox(root)
 # for item in mp3:  # 第二个小部件插入数据
 #     searchList.insert(0, item)
 searchList.pack()
 
-
-
-
-
-
-
-
-
 # print(search.searchMusic('我们不一样'))
-
 
 root.mainloop()  # 进入消息循环
