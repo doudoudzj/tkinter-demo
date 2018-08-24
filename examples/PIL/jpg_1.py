af91409c5f001f635e8a294db53c20365a38b938
唐钰豆豆 <doudoudzj@sina.com>
2018-08-24 18:20:27 +0800

    update

--- examples/PIL/jpg_1.py
+++ examples/PIL/jpg_1.py
@@ -3,7 +3,7 @@
 """加载jpg图片"""
 
 import os
-from tkinter import Tk, Button, Entry, Label
+from tkinter import Tk, Label
 
 from PIL import Image, ImageTk
 
@@ -15,12 +15,6 @@ image_file = os.path.join(os.path.dirname(__file__), "doudou.jpg")
 img = Image.open(image_file)  # 打开图片
 photo = ImageTk.PhotoImage(img)  # 用PIL模块的PhotoImage打开
 imglabel = Label(root, image=photo)
-imglabel.grid(row=0, column=0, columnspan=4)
-
-Label(root, text="输入:").grid(row=1, column=0, sticky='sn')
-
-Entry(root).grid(row=1, column=1, columnspan=2)
-
-Button(root, text="确定").grid(row=1, column=3)
+imglabel.pack()
 
 root.mainloop()
